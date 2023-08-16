c3baea7d9012d400c689f23dc433da7519411e0c
Klaudia Rutkowska <rutkowskaklaudia731@gmail.com>
2023-08-16 11:18:12 +0200

    Added into section

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,25 @@ module.exports = {
             'sans': ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', '"Noto Sans"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
             'inter': ['Inter', 'sans-serif'],
             'montserrat': ['Montserrat', 'sans-serif'],
+        },
+
+        extend: {
+            colors: {
+                'bright': '#F5F0EC',
+                'beige': '#DCC1AB',
+                'green': '#1B5B31',
+            },
+            spacing: {  
+                '0.3': '5px',
+                '1.4': '22px',
+                '4.5': '72px',
+                '5.5': '89px'
+            },
+            borderRadius: {
+                button: '200px'
+            },
         }
+
     },
     plugins: [],
 }
