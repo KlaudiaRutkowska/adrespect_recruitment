9625fcac93cbb316fad742946b3e5ed845999d21
Klaudia Rutkowska <rutkowskaklaudia731@gmail.com>
2023-08-16 18:32:15 +0200

    Added offer section

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,13 +14,36 @@ module.exports = {
         },
         container: {
             center: true,
+            padding: '89px'
         },
         fontFamily: {
             'sans': ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', '"Noto Sans"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
             'inter': ['Inter', 'sans-serif'],
             'montserrat': ['Montserrat', 'sans-serif'],
+        },
+
+        extend: {
+            colors: {
+                'bright': '#F5F0EC',
+                'beige': '#DCC1AB',
+                'green': '#1B5B31',
+            },
+            spacing: {  
+                '0.3': '5px',
+                '1.4': '22px',
+                '4.5': '72px',
+                '5.5': '89px',
+                '30': '120px'
+            },
+            borderRadius: {
+                'border-container': '28px',
+                button: '200px'
+            },
+            fontSize: {
+                '1.75': '28px'
+            }
         }
+
     },
     plugins: [],
 }
-
