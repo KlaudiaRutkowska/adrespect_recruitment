8a06add7ff75c92f1e982862de932f62873edb34
Klaudia Rutkowska <rutkowskaklaudia731@gmail.com>
2023-08-20 15:30:02 +0200

    Added responses

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,19 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
         screens: {
-            'sm': '576px',
-            // => @media (min-width: 576px) { ... }
+            'sm': '640px',
+            // => @media (min-width: 640px) { ... }
 
-            'md': '960px',
-            // => @media (min-width: 960px) { ... }
+            'md': '768px',
+            // => @media (min-width: 768px) { ... }
 
-            'lg': '1440px',
+            'lg': '1024px',
+            // => @media (min-width: 1024px) { ... }
+
+            'xl': '1280px',
+            // => @media (min-width: 1280px) { ... }
+
+            '2xl': '1440px',
             // => @media (min-width: 1440px) { ... }
         },
         container: {
